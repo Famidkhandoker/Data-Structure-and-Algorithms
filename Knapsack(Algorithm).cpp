0f479fa0d8644668e8110146c0d498cc3c99b581
FAMID KHANDOKER <famidkhandoker@gmail.com>
2025-09-04 11:19:12 +0600

    Add files via upload

---
+++ Knapsack(Algorithm).cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+struct Item
+{
+    int weight,value;
+};
+void knapsack(Item arr[],int n,int W)
+{
+    int dp [n+1][W+1];
+    for(int i=0; i<=n; i++)
+    {
+        for(int w=0; w<=W; w++)
+        {
+            if(i==0||w==0)
+            {
+                dp[i][w]=0;
+            }
+            else if(arr[i-1].weight<=w)
+            {
+                dp[i][w]=max(arr[i-1].value+dp[i-1][w-arr[i-1].weight],dp[i-1][w]);
+            }
+            else
+            {
+                dp[i][w]=dp[i-1][w];
+            }
+        }
+    }
+    cout<<"Maximum value in Knapsack ="<<dp[n][W];
+    cout<<" "<<endl;
+    cout<<"Items included(Weight,value):";
+    int res;
+    res=dp[n][W];
+    int w=W;
+    for(int i=n; i>0 && res>0; i--)
+    {
+        if(res<=dp[i-1][w])
+        {
+            continue;
+        }
+        else
+        {
+            cout<< "("<<arr[i-1].weight<<","<<arr[i-1].value<<")";
+            res-=arr[i-1].value;
+            w-=arr[i-1].weight;
+
+        }
+    }
+}
+int main()
+{
+    Item arr[]= {{2,12},{1,10},{3,20},{ 2,15}};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int W=5;
+    cout<<n<<endl;
+    knapsack(arr,n,W);
+}
+
---
+++ Rod_Cutting(Algorithm).cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+struct RodPiece
+{
+    int length,profit;
+};
+void rodCutting(RodPiece arr[],int n,int N)
+{
+    int dp[n+1][n+1];
+    int i,j;
+    for(i=0; i<=n; i++)
+    {
+        for(j=0; j<=N; j++)
+        {
+            if(i==0||j==0)
+            {
+                dp[i][j]=0;
+            }
+            else if(arr[i-1].length<=j)
+            {
+                dp[i][j]=max(arr[i-1].profit+dp[i][j-arr[i-1].length],dp[i-1][j]);
+            }
+            else
+            {
+                dp[i][j]=dp[i-1][j];
+            }
+        }
+    }
+
+    int res;
+    res=dp[n][N];
+    i=n;
+    j=N;
+    int cuts[N];
+    int cutCount=0;
+    while(i>0&&res>0)
+    {
+        if(dp[i][j]==dp[i-1][j])
+        {
+            i--;
+        }
+        else
+        {
+            cuts[cutCount]=arr[i-1].length;
+            cutCount=cutCount+1;
+            res-=arr[i-1].profit;
+            j-=arr[i-1].length;
+        }
+    }
+    cout<<"Maximum Obtainable Value = "<<dp[n][N]<<endl;
+    cout<<"Cut sizes:";
+    for(int k=0; k<cutCount; k++)
+    {
+        cout<<cuts[k]<<" ";
+    }
+}
+
+int main()
+{
+    RodPiece rr[]= {{1,1}, {2,5}, {3,8}, {4,9}, {5,10}, {6,17}, {7,17}, {8,20}};
+    int n =sizeof(rr)/sizeof(rr[0]);
+    int N=8;
+    cout<<n<<endl;
+    rodCutting(rr,n,N);
+
+}
+
